6a9f23ea03a6029d3a1115d2c1bf9e3347bbbe1c
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-10-04 06:30:06 +0700

    added router kitsu

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,7 @@ const User = require('./User')
 const Home = require('./Home')
 
 Router.use('/users', User)
-<<<<<<< HEAD
 Router.use('/home', Home)
-=======
 Router.use('/kitsu', kitsu)
->>>>>>> added kitsu
 
 module.exports = Router;
