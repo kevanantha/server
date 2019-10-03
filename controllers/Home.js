f018562ed5fa6cf3ba04d37707de07d0889567b3
Kevin Anantha <kevin.anantha@gmail.com>
2019-10-04 02:37:56 +0700

    :sparkles: upcoming endpoint

--- controllers/Home.js
+++ controllers/Home.js
@@ -1,16 +1,27 @@
 const Jikan = require('../apis/jikan')
 
 class HomeController{
-    static upcoming(req, res, next){
-        Jikan({
+    static async upcoming(req, res, next){
+      try {
+        const { data: upcoming } = await Jikan({
+          method: 'get',
+          url: 'top/anime/1/upcoming'
+        })
+
+        const data = await upcoming.top.map(async up => {
+          const { data: detail } = await Jikan({
             method: 'get',
-            url: 'top/anime/1/upcoming'
+            url: `anime/${up.mal_id}`
+          })
+          return detail
         })
-        .then(({data}) => {
-            console.log(data);
-            res.status(200).json(data)
-        })
-        .catch(next)
+
+        const all = await Promise.all(data)
+
+        res.status(200).json(all)
+      } catch(err) {
+        next()
+      }
     }
 }
 
